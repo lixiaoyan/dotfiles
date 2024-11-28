74c4731c682a2f7b4a941b9f65a678a08d585d20
Xiaoyan Li <lxy.lixiaoyan@gmail.com>
2024-11-28 16:43:35 +0800

    add hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,22 @@
+local function toggleApp(id)
+  local app = hs.application.get(id)
+  if app == nil then
+    hs.application.open(id)
+    return
+  end
+  if app:isFrontmost() then
+    app:hide()
+  else
+    app:activate()
+  end
+end
+
+local function registerAppHotkey(id, mods, key)
+  hs.hotkey.bind(mods, key, function()
+    toggleApp(id)
+  end)
+end
+
+registerAppHotkey("co.zeit.hyper", { "cmd", "ctrl" }, "t")
+
+hs.pathwatcher.new(hs.configdir, hs.reload):start()
