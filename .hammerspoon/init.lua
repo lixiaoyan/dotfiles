be977bde0c23d2501046c2e3b9e0b897b89de4ed
Xiaoyan Li <lxy.lixiaoyan@gmail.com>
2024-11-29 20:15:46 +0800

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,7 +3,7 @@ function watchers:add(watcher)
   table.insert(self, watcher)
 end
 
-require("app-hotkeys")
-require("macos-focus-workaround")
-
 watchers:add(hs.pathwatcher.new(hs.configdir, hs.reload):start())
+
+require("app-hotkeys")
+require("focus-workaround")
