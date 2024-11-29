b8dd2d4b8ad0d08db10b32b1c64850770d87c632
Xiaoyan Li <lxy.lixiaoyan@gmail.com>
2024-11-29 19:56:25 +0800

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,60 +1,9 @@
-local function toggleApp(id)
-  local app = hs.application.get(id)
-  if app == nil then
-    hs.application.open(id)
-    return
-  end
-  if app:isFrontmost() then
-    app:hide()
-  else
-    app:activate()
-  end
+watchers = {}
+function watchers:add(watcher)
+  table.insert(self, watcher)
 end
 
-local function registerAppHotkey(id, mods, key)
-  hs.hotkey.bind(mods, key, function()
-    toggleApp(id)
-  end)
-end
+require("app-hotkeys")
+require("macos-focus-workaround")
 
-registerAppHotkey("co.zeit.hyper", { "cmd", "ctrl" }, "t")
-
-local lastActivatedApp = nil
-local restoreActivationTimer = hs.timer.delayed.new(0.1, function()
-  if lastActivatedApp ~= nil then
-    lastActivatedApp:activate()
-  end
-end)
-
-hs.application.watcher
-  .new(function(name, event, app)
-    if event == hs.application.watcher.activated then
-      if name == "WindowManager" then
-        restoreActivationTimer:start()
-      else
-        lastActivatedApp = app
-      end
-    end
-  end)
-  :start()
-
-local lastFocusedWindowBySpace = {}
-
-hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function(window, name)
-  lastFocusedWindowBySpace[hs.spaces.focusedSpace()] = window
-end)
-
-hs.spaces.watcher
-  .new(function()
-    lastActivatedApp = nil
-    restoreActivationTimer:stop()
-    if hs.application.frontmostApplication():name() == "WindowManager" then
-      lastFocusedWindow = lastFocusedWindowBySpace[hs.spaces.focusedSpace()]
-      if lastFocusedWindow ~= nil then
-        lastFocusedWindow:focus()
-      end
-    end
-  end)
-  :start()
-
-hs.pathwatcher.new(hs.configdir, hs.reload):start()
+watchers:add(hs.pathwatcher.new(hs.configdir, hs.reload):start())
---
+++ .hammerspoon/macos-focus-workaround.lua
@@ -0,0 +1,42 @@
+local lastActivatedApp = nil
+local restoreActivationTimer = nil
+local lastFocusedWindowBySpace = {}
+
+watchers:add(hs.application.watcher
+  .new(function(name, event, app)
+    if event == hs.application.watcher.activated then
+      if name == "WindowManager" then
+        restoreActivationTimer = hs.timer.doAfter(0.1, function()
+          if lastActivatedApp ~= nil then
+            lastActivatedApp:activate()
+          end
+        end)
+      else
+        lastActivatedApp = app
+      end
+    end
+  end)
+  :start())
+
+hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function(window, name)
+  for _, space in ipairs(hs.spaces.windowSpaces(window)) do
+    lastFocusedWindowBySpace[space] = window
+  end
+end)
+
+watchers:add(hs.spaces.watcher
+  .new(function()
+    lastActivatedApp = nil
+    if restoreActivationTimer ~= nil then
+      restoreActivationTimer:stop()
+      restoreActivationTimer = nil
+    end
+
+    if hs.application.frontmostApplication():name() == "WindowManager" then
+      lastFocusedWindow = lastFocusedWindowBySpace[hs.spaces.focusedSpace()]
+      if lastFocusedWindow ~= nil then
+        lastFocusedWindow:focus()
+      end
+    end
+  end)
+  :start())
