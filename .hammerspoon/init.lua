4656bff03119f1db57d80f5b1d2ffb110e212ba8
Xiaoyan Li <lxy.lixiaoyan@gmail.com>
2024-12-03 11:47:47 +0800

    update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,7 +4,8 @@ watchers:add(hs.pathwatcher.new(hs.configdir, hs.reload):start())
 
 registerAppHotkey("co.zeit.hyper", { "cmd", "ctrl" }, "t")
 
-registerMediaHotkey("PREVIOUS", { "cmd", "ctrl" }, "left")
-registerMediaHotkey("NEXT", { "cmd", "ctrl" }, "right")
+registerMediaHotkey("PLAY", { "cmd", "ctrl" }, "/")
+registerMediaHotkey("PREVIOUS", { "cmd", "ctrl" }, ",")
+registerMediaHotkey("NEXT", { "cmd", "ctrl" }, ".")
 
 require("focus-workaround")
