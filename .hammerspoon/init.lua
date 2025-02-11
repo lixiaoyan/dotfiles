297d5065413e60f49e6a71a0b7e17711eace39ec
Xiaoyan Li <lxy.lixiaoyan@gmail.com>
2025-02-11 18:49:07 +0800

    Update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,4 +4,19 @@ registerMediaHotkey("PLAY", { "cmd", "ctrl" }, "/")
 registerMediaHotkey("PREVIOUS", { "cmd", "ctrl" }, ",")
 registerMediaHotkey("NEXT", { "cmd", "ctrl" }, ".")
 
+hs.hotkey.bind({ "alt" }, "space", function()
+  local id = "com.mitchellh.ghostty"
+  local menuItem = { "View", "Quick Terminal" }
+  local app = hs.application.get(id)
+  if app == nil then
+    hs.execute("open -a Ghostty --background --args --initial-window=false", true)
+    app = hs.application.get(id)
+  end
+  local result = app:selectMenuItem(menuItem)
+  if result == nil then
+    hs.timer.usleep(1e5)
+    app:selectMenuItem(menuItem)
+  end
+end)
+
 require("focus-workaround")
